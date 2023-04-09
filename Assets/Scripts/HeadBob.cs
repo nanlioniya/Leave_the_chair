34f78830fe6d171ff5bfce94ef290ccda5a66cdd
Jiiimp <o19990311@gmail.com>
2023-04-09 20:55:23 +0800

    combine current progress again

--- Assets/Scripts/HeadBob.cs
+++ Assets/Scripts/HeadBob.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class HeadBob : MonoBehaviour
 {
     public bool enable = true;
-    public float xUpDown = 0.01f;
-    public float yUp = 0.02f;
-    public float yDown = 0.02f;
-    public float frequency = 3f;
+    public float xAmplitude = 0.015f;
+    public float yAmplitude = 0.015f;
+    public float frequency = 10f;
 
 
     private void Update()
@@ -17,29 +16,15 @@ public class HeadBob : MonoBehaviour
         {
             Vector3 pos = Vector3.zero;
             pos += FootStepMotion();
-            transform.localPosition += pos;
+            transform.position += pos;
         }
     }
 
     private Vector3 FootStepMotion()
     {
         Vector3 pos = Vector3.zero;
-
-        // effect 1
-        //if (Mathf.Sin(Time.time * frequency) > 0)
-        //{
-        //    pos.y += Mathf.Sin(Time.time * frequency * yDown / (yUp + yDown)) * yUp;
-        //}
-        //else
-        //{
-        //    pos.y += Mathf.Sin(Time.time * frequency * yDown / (yUp + yDown)) * yDown;
-        //}
-        //pos.x += Mathf.Cos(Time.time * frequency / 2) * xUpDown;
-
-        // effect 2
-        pos.y += Mathf.Sin(Time.time * frequency) * yUp;
-        pos.x += Mathf.Cos(Time.time * frequency / 2) * xUpDown;
-
+        pos.y += Mathf.Sin(Time.time * frequency) * yAmplitude;
+        pos.x += Mathf.Cos(Time.time * frequency / 2) * xAmplitude;
         return pos;
     }
 }
